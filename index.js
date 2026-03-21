64cbd8cbfce308256384b0b11fd860ecd927b848
adunato <daniele.torrigiani@gmail.com>
2026-03-21 14:38:14 +0000

    Chore: Add direct console logs to index.js to verify loading and hook execution

--- index.js
+++ index.js
@@ -6,23 +6,32 @@ import { startButtonMount } from './src/ui/buttons.js';
 import { applyInjection, filterContextInterceptor } from './src/core/injector.js';
 import { eventSource, event_types } from '../../../../scripts/events.js';
 
+console.log(`[${extensionName}] Loading index.js...`);
+
 function onChatChanged() {
+    console.log(`[${extensionName}] [Event] onChatChanged triggered`);
     logDebug('log', 'Chat changed, refreshing chat-scoped state');
     updateSettingsUI(state.settingsContainer);
     applyInjection();
 }
 
 jQuery(async () => {
+    console.log(`[${extensionName}] jQuery document ready. Initializing...`);
     ensureSettings();
     await mountSettings();
     startButtonMount();
     try {
         eventSource?.on(event_types.CHAT_CHANGED, onChatChanged);
-        logDebug('log', 'Registered prompt filter listeners (migrated to generate_interceptor)');
+        console.log(`[${extensionName}] Registered CHAT_CHANGED listener.`);
     } catch (err) {
-        console.error(`[${extensionName}] Failed to register prompt filter:`, err);
+        console.error(`[${extensionName}] Failed to register event listeners:`, err);
     }
 });
 
 // Expose the interceptor globally matching the name in manifest.json
-window['SceneSummariser_filterContextInterceptor'] = filterContextInterceptor;
+window['SceneSummariser_filterContextInterceptor'] = function(...args) {
+    console.log(`[${extensionName}] [Hook] SceneSummariser_filterContextInterceptor called by SillyTavern!`);
+    return filterContextInterceptor(...args);
+};
+console.log(`[${extensionName}] Exposed SceneSummariser_filterContextInterceptor to window.`);
+
